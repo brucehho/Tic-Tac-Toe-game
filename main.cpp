3d24ec84bf5e5b061a1e476d9d9a814ee7e15c25
HONGBINHE-city <HHBdefinition@hotmail.com>
2021-04-16 15:54:38 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Tic-Tac-Toe 2.0 - multiple fi les
+// Main function
+#include <iostream>
+//for access to Game class defi nition
+#include "game.h"
+using namespace std;
+int main()
+{
+	Game ticTacToe;
+	ticTacToe.DisplayInstructions();
+	char again;
+	do
+	{
+		ticTacToe.Play();
+		cout << endl << "Play again? (y/n): ";
+		cin >> again;
+	} while (again != 'n');
+	return 0;
+}
---
+++ player.h
@@ -0,0 +1,24 @@
+#pragma once
+// Tic-Tac-Toe 2.0 - multiple files
+// Player definition - class represents a tic-tac-toe player
+
+//part of include guard - tests if PLAYER_H not defi ned
+#ifndef PLAYER_H
+//part of include guard - defi nes PLAYER_H
+#define PLAYER_H
+//forward declaration of Board class
+class Board;
+class Player
+{
+public:
+	Player();
+	char GetPiece() const;
+	void MakeMove(Board& aBoard) const;
+private:
+	static const int NUM_PIECES = 2;
+	static const char PIECES[NUM_PIECES];
+	static int current;
+	char m_Piece;
+};
+//part of include guard - ends #ifndef
+#endif
